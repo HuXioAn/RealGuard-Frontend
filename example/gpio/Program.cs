7d22c372a186f410c48e07e75f299276b8c6f533
HuXioAn <hu7006@outlook.com>
2023-04-21 20:00:05 +0800

    gpio

--- example/gpio/Program.cs
+++ example/gpio/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+using System.Device.Gpio;
+
+
+
+int pin = 18;
+using var controller = new GpioController();
+controller.OpenPin(pin, PinMode.Output);
+bool ledOn = true;
+while (true)
+{
+    controller.Write(pin, ((ledOn) ? PinValue.High : PinValue.Low));
+    Thread.Sleep(1000);
+    ledOn = !ledOn;
+}
