317d0ad4fcaf9019eddab0177fe7cadd7263bbd2
HuXioAn <hu7006@outlook.com>
2023-04-21 21:57:59 +0800

    回调

--- example/gpio/Program.cs
+++ example/gpio/Program.cs
@@ -3,7 +3,7 @@ using static System.Console;
 
 using System.Device.Gpio;
 
-
+var dele = new PinChangeEventHandler(isr);
 
 //IO使用sys定义
 int i = 18;
@@ -12,8 +12,8 @@ int k = 79;
 using var controller = new GpioController();
 
 controller.OpenPin(i, PinMode.Output);
-controller.OpenPin(k,PinMode.Input);
-
+controller.OpenPin(k,PinMode.Input,PinValue.High);
+controller.RegisterCallbackForPinValueChangedEvent(k,PinEventTypes.Falling,dele);
 
 while (true)
 {
@@ -27,4 +27,10 @@ while (true)
     WriteLine("Pin{0}:level{1}",k,controller.Read(k));
 
     
+}
+
+
+
+void isr(object sender, PinValueChangedEventArgs pinValueChangedEventArgs){
+    WriteLine("Triggered");
 }
