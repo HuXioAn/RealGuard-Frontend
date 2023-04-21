7ad745523c0409e06648a30dfddb2ccc2c3a2281
HuXioAn <hu7006@outlook.com>
2023-04-21 16:47:14 +0800

    imagesharp输出成功

--- example/cap/Program.cs
+++ example/cap/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Intel.RealSense;
 using static System.Console;
-using System.Drawing;
-
+using System.Runtime.InteropServices;
 using SixLabors.ImageSharp;
 
 
@@ -22,25 +21,26 @@ using (var ctx = new Context())
 var config = new Config();
 config.EnableStream(Intel.RealSense.Stream.Depth,640,480,Format.Z16,15);
 //config.EnableStream(Intel.RealSense.Stream.Infrared,Format.Y16);
-config.EnableStream(Intel.RealSense.Stream.Infrared,WIDTH,HEIGHT,Format.Bgr8,15);
+config.EnableStream(Intel.RealSense.Stream.Infrared,1,WIDTH,HEIGHT,Format.Y8,15);//左眼
+config.EnableStream(Intel.RealSense.Stream.Infrared,2,WIDTH,HEIGHT,Format.Y8,15);//右眼
 
 //config.EnableStream(Intel.RealSense.Stream.Infrared,1);
 
 var pipe = new Pipeline();
 PipelineProfile selection = pipe.Start(config);
 
-// var selected_device = selection.Device;
-// var depth_sensor = selected_device.Sensors[0];
+var selected_device = selection.Device;
+var depth_sensor = selected_device.Sensors[0];
 
-// if (depth_sensor.Options.Supports(Option.EmitterEnabled))
-// {
-//     depth_sensor.Options[Option.EmitterEnabled].Value = 1f; // Enable emitter
-//     //depth_sensor.Options[Option.EmitterEnabled].Value = 0f; // Disable emitter
-//     // Thread.Sleep(1000);
-//     // depth_sensor.Options[Option.EmitterEnabled].Value = 0f; // Disable emitter
-//     // Thread.Sleep(1000);
-//     // depth_sensor.Options[Option.EmitterEnabled].Value = 1f;
-// }
+if (depth_sensor.Options.Supports(Option.EmitterEnabled))
+{
+    depth_sensor.Options[Option.EmitterEnabled].Value = 0f; // Enable emitter
+    //depth_sensor.Options[Option.EmitterEnabled].Value = 0f; // Disable emitter
+    // Thread.Sleep(1000);
+    // depth_sensor.Options[Option.EmitterEnabled].Value = 0f; // Disable emitter
+    // Thread.Sleep(1000);
+    // depth_sensor.Options[Option.EmitterEnabled].Value = 1f;
+}
 
 Colorizer color_map = new Colorizer();
 
@@ -58,15 +58,21 @@ using(var frames = pipe.WaitForFrames()){
 
 
     var irFrame = frames.InfraredFrame.DisposeWith(frames);
-    Bitmap irImg = new System.Drawing.Bitmap(irFrame.Width, irFrame.Height, irFrame.Stride, System.Drawing.Imaging.PixelFormat.Format24bppRgb, irFrame.Data);
+    //Bitmap irImg = new System.Drawing.Bitmap(irFrame.Width, irFrame.Height, irFrame.Stride, System.Drawing.Imaging.PixelFormat.Format8bppIndexed, irFrame.Data);
     //Bitmap irImg = new System.Drawing.Bitmap(irFrame.Height, irFrame.Width, irFrame.Stride, System.Drawing.Imaging.PixelFormat.Format24bppRgb, irFrame.Data);
     
     //var img = SixLabors.ImageSharp.Image.Load();
+    var irByte = new byte[irFrame.DataSize];
+    Marshal.Copy(irFrame.Data,irByte,0,irFrame.DataSize);
+    
+    //var irImg = Image.Load(irByte);
+    var irImg = Image.LoadPixelData<L8>(irByte,irFrame.Width,irFrame.Height);
     
     WriteLine("depthFrame:Height:{0},Width:{1}",irImg.Height,irImg.Width);
     
-    Bitmap newImage = new Bitmap(irImg);
-    newImage.Save("./pic/ir.png",System.Drawing.Imaging.ImageFormat.Png);
+    irImg.Save("./pic/irImg.jpg");
+    //Bitmap newImage = new Bitmap(irImg);
+    //newImage.Save("./pic/ir.png",System.Drawing.Imaging.ImageFormat.Png);
     WriteLine("irFrame:Height:{0},Width:{1}",irFrame.Height,irFrame.Width);
 
     
