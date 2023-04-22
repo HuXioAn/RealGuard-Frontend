244242333a59b0de48594b0ad55d59a3dd83c751
胡小安 <hu7006@outlook.com>
2023-04-22 13:46:40 +0800

    grpc 没搞懂

--- example/grpc/Program.cs
+++ example/grpc/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using static System.Console;
-
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using grpcClient;
 
 
 
 namespace rpc{
     public class exampleRPC{
         static public void Main(){
+
+            // The port number must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:7042");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                            new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
             
         }
     }
