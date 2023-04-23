754172926faaebee4ffadde2281efc17bfcf2a28
huxiaoan <hu7006@outlook.com>
2023-04-23 21:38:35 +0800

    告一段落

--- frontEnd/Program.cs
+++ frontEnd/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using static System.Console;
 using realSense;
+using grpcClient;
+using System.Device.Gpio;
 
 
 
@@ -8,12 +10,23 @@ namespace realGuardFrontEnd{
 
     public class frontEnd{
 
+
+
         public static void Main(){
 
             
 
 
 
+        }
+
+
+        private static void systemInit(string address){
+            //Grpc客户端 realSense Gpio
+            rpcClient = new grpcClient.grpcClient(address);
+            cam = new realSense.realSense();
+
+            
         }
 
 
