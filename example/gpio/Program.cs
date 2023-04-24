aaa4b39ff7c321b001439d297914c64de3dd76fb
胡小安 <hu7006@outlook.com>
2023-04-24 23:11:03 +0800

    sensor test

--- example/gpio/Program.cs
+++ example/gpio/Program.cs
@@ -8,18 +8,12 @@ using realGuardGpio;
 
 var ioController = new realGuardGpio.realGuardGpio(18,200);
 
-ioController.bodySensorCallbackRegister(false,isr);
+ioController.bodySensorCallbackRegister(true,isr);
 
 
 while (true)
 {
 
-
-    //controller.Write(i, PinValue.High);
-    ioController.gateIoSet(true);
-    Thread.Sleep(1000);
-    //controller.Write(i, PinValue.Low);
-    ioController.gateIoSet(false);
     Thread.Sleep(1000);
 
     WriteLine("Pin:level{0}",ioController.bodySensorRead());
