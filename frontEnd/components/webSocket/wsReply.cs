34a31c7d8564aa16bf69e2499f77a40ec7d80619
huxiaoan <hu7006@outlook.com>
2023-05-28 22:52:40 +0800

    websocket json协议说明注释

--- frontEnd/components/webSocket/wsReply.cs
+++ frontEnd/components/webSocket/wsReply.cs
@@ -18,7 +18,13 @@ namespace realWebSocketServer{
 
 
     public class wsReplyAuth : wsReply{
-        //state: authOk authFail
+        /*
+            state: authOk authFail
+            requestId: 重复web端发来的id
+            username: 重复认证收到的用户名，失败为空
+            token: 认证通过后分发的token，失败为空
+        */
+
         public string username{set; get;}
         public string token{set; get;}
 
@@ -26,28 +32,48 @@ namespace realWebSocketServer{
 
 
     public class wsReplyRegister : wsReply{
-        //state: ready
+        /*
+            state: ready
+            requestId: 重复web端发来的id
+            name: 重复收到的录入名称
+        */
+
         public string name{set; get;}
     }
 
 
     public class wsReplySnap : wsReply{
-        //state: ok fail
-        //base64 : picBase64 errMsg
+        /*
+            state: ok fail
+            requestId: 重复web端发来的id
+            picBase64: 图片base64文本 失败情况下为errMsg
+            picId: 代表图片的id，由前端给出
+            dist: 对于拍摄的照片的验证，在特殊情况（错误、第一张、未检测到）下为-1，其余应0~1
+        */
+
         public string picBase64{set; get;}
         public int picId{set; get;}
         public float dist{set; get;}
     }
 
     public class wsReplyCheck : wsReply{
-        //state : ok fail
+        /*
+            state: ok fail
+            requestId: 重复web端发来的id
+            picId: 代表图片的id，由前端拍摄时给出
+            result: 重复web端发来的判决结果 accept reject
+        */
+
         public int picId{set; get;}
         public string result{set; get;}
     }
 
 
     public class wsReplyOver : wsReply{
-        //state : over
+        /*
+            state: ok fail
+            requestId: 重复web端发来的id
+        */
     }
 
 
--- frontEnd/components/webSocket/wsRequest.cs
+++ frontEnd/components/webSocket/wsRequest.cs
@@ -19,6 +19,11 @@ namespace realWebSocketServer{
 
 
     public class wsRequestAuth : wsRequest{
+        /*
+            request : auth
+            requestId : 由web端生成
+            token : 留空字符串
+        */
         public string username{set; get;}
         public string password{set; get;}
 
@@ -26,6 +31,11 @@ namespace realWebSocketServer{
 
 
     public class wsRequestRegister : wsRequest{
+        /*
+            request : register
+            requestId : 由web端生成
+            token : 被分到的token
+        */
         public string name{set; get;}
         public string studentId{set; get;}
 
@@ -33,17 +43,34 @@ namespace realWebSocketServer{
 
 
     public class wsRequestSnap : wsRequest{
+        /*
+            request : snap
+            requestId : 由web端生成
+            token : 被分到的token
+        */
 
     }
 
 
     public class wsRequestCheck : wsRequest{
+        /*
+            request : check
+            requestId : 由web端生成
+            token : 被分到的token
+            picId : 在snap的reply中由前端给出的picId，表征上一张照片
+            result : 由用户给出的判别 通过accept 不通过reject
+        */
         public int picId{set; get;}
         public string result{set; get;} //accept reject
     }
 
 
     public class wsRequestOver : wsRequest{
+        /*
+            request : over
+            requestId : 由web端生成
+            token : 被分到的token
+        */
         
     }
 
